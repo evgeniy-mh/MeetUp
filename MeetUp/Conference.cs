78f1593aef9fa965ee5e90011981cf8d8ac59103
evgeniy-mh <djpon94@gmail.com>
2018-02-05 13:09:55 +0400

    added Conference Class

---
+++ MeetUp/Conference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetUp
+{
+    class Conference
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+        public Conference()
+        {
+            Employees = new List<Employee>();
+        }
+    }
+}
--- MeetUp/Employee.cs
+++ MeetUp/Employee.cs
@@ -10,11 +10,17 @@ namespace MeetUp
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        /*public string MiddleName { get; set; }
+        public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
         public string TelephoneNumber { get; set; }
-        public string Email { get; set; }*/
+        public string Email { get; set; }
+
+        public ICollection<Conference> Conferences { get; set; }
+        public Employee()
+        {
+            Conferences = new List<Conference>();
+        }
     }
 }
