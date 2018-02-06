23f15309650b78486e8a55976cfa9a4924424b3b
evgeniy-mh <djpon94@gmail.com>
2018-02-06 14:55:13 +0400

    added Main menu giu

---
+++ MeetUp/View/MainMenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MeetUp.View
+{
+    class MainMenuItem
+    {
+        public string Name { get; set; }
+        public UIElement View { get; set; }
+    }
+}
--- MeetUp/View/MainWindow.xaml.cs
+++ MeetUp/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeetUp.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,13 +18,30 @@ namespace MeetUp
 {
     public partial class MainWindow : Window
     {
+        List<MainMenuItem> menuItems = new List<MainMenuItem> {
+            new MainMenuItem { Name = "Сотрудники", View = new UserControl1() },
+            new MainMenuItem { Name = "Совет", View = new ConferenceControll() }
+            };
+
+        MainMenuItem selectedMenuItem { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //DataContext = new EmployeesVM();
+            MenuItemsListBox.ItemsSource = menuItems;
+            MenuItemsListBox.SelectedItem = selectedMenuItem;
+            MenuItemsListBox.SelectedIndex = 0;
 
-            MainView.Children.Add(new UserControl1());
+            //DataContext = new EmployeesVM();
+            //MainView.Children.Add(new UserControl1());
+        }
+
+        public void MenuSelector_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            MainView.Children.Clear();
+            MainMenuItem item = MenuItemsListBox.SelectedItem as MainMenuItem;
+            MainView.Children.Add(item.View);
         }
     }
 }
