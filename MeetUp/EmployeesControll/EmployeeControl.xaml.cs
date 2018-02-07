9d71843556c7a719cbf106c683ec4994770b098e
evgeniy-mh <djpon94@gmail.com>
2018-02-07 12:47:39 +0400

    changed project folders

--- MeetUp/EmployeesControll/EmployeeControl.xaml.cs
+++ MeetUp/EmployeesControll/EmployeeControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MeetUp.View
+namespace MeetUp.EmployeesControl
 {
     /// <summary>
     /// Interaction logic for EmployeeControl.xaml
