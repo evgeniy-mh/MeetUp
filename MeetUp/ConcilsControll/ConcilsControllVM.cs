ea94917c9a7b2e75bb5b25ec84b4165be9017bde
evgeniy-mh <djpon94@gmail.com>
2018-02-08 20:47:59 +0400

    added template for concil commands

--- MeetUp/ConcilsControll/ConcilsControllVM.cs
+++ MeetUp/ConcilsControll/ConcilsControllVM.cs
@@ -31,6 +31,65 @@ namespace MeetUp.ConcilsControll
             Concils = new ObservableCollection<Concil>(ConcilRepository.Get());
         }
 
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(obj =>
+                {
+                    /*EmployeeWindowView window = new EmployeeWindowView();
+                    if (window.ShowDialog() == true)
+                    {
+                        EmployeeRepository.Create(window.Employee);
+                        Employees = new ObservableCollection<Employee>(EmployeeRepository.Get());
+                    }*/
+                }));
+            }
+        }
+
+        private RelayCommand changeCommand;
+        public RelayCommand ChangeCommand
+        {
+            get
+            {
+                return changeCommand ?? (changeCommand = new RelayCommand(obj =>
+                {
+                    /*EmployeeWindowView window = new EmployeeWindowView(SelectedEmployee);
+                    if (window.ShowDialog() == true)
+                    {
+                        EmployeeRepository.Update(window.Employee);
+                        Employees = new ObservableCollection<Employee>(EmployeeRepository.Get());
+                    }*/
+                }, (obj) => { return SelectedConcil != null; }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ?? (removeCommand = new RelayCommand(obj =>
+                {
+                    /*MessageBoxButton button = MessageBoxButton.OKCancel;
+                    MessageBoxImage icon = MessageBoxImage.Warning;
+                    MessageBoxResult result = MessageBox.Show(
+                        String.Format("Вы действительно хотите удалить сотрудника {0} {1} {2} ?",
+                        SelectedEmployee.MiddleName,
+                        SelectedEmployee.FirstName,
+                        SelectedEmployee.LastName),
+                        "Удалить сотрудника?", button, icon);
+
+                    if (result == MessageBoxResult.OK)
+                    {
+                        EmployeeRepository.Remove(SelectedEmployee);
+                        Employees = new ObservableCollection<Employee>(EmployeeRepository.Get());
+                    }*/
+                }, (obj) => { return SelectedConcil != null; }));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName]string prop = "")
         {
